89070441bcc0336edd5f9eb8b6089f45d036027e
Federico <federico.creti87@google.com>
2018-04-28 10:08:16 +0200

    Added Java library

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,72 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.udacity.jokesprovider.JokesProvider;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    @BindView(R.id.joke)
+    TextView joke;
+
+    @BindView(R.id.say_a_joke)
+    Button sayAJokeButton;
+
+    @BindView(R.id.adView)
+    AdView mAdView;
+
+    Context context;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        this.context = context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ButterKnife.bind(this, root);
+
+        //AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        sayAJokeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JokesProvider jokesProvider = new JokesProvider();
+                joke.setText(jokesProvider.getJoke());
+            }
+        });
+
+        return root;
+    }
+}
---
+++ jokesprovider/src/main/java/com/example/udacity/jokesprovider/JokesProvider.java
@@ -0,0 +1,11 @@
+package com.example.udacity.jokesprovider;
+
+public class JokesProvider {
+    public String getJoke(){
+        return "Teacher: Maria please point to America on the map. \n" +
+                "Maria: This is it. \n" +
+                "Teacher: Well done. Now class, who found America? \n" +
+                "Class: Maria did.\n\n\n" +
+                "I'm so sad :(";
+    }
+}
